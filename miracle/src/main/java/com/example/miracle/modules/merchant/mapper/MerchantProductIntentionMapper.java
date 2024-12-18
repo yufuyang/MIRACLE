eed7fcfeb5a943a76b44ca8f3061c010c61a2c74
yfy <yufuyang@sovell.com>
2024-12-18 10:43:42 +0800

    【add】管理端 公司端

---
+++ miracle/src/main/java/com/example/miracle/modules/merchant/mapper/MerchantProductIntentionMapper.java
@@ -0,0 +1,12 @@
+package com.example.miracle.modules.merchant.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.miracle.modules.merchant.entity.MerchantProductIntention;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 商户产品意向Mapper
+ */
+@Mapper
+public interface MerchantProductIntentionMapper extends BaseMapper<MerchantProductIntention> {
+} 
